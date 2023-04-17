6ceb257074ec5fb95c3e9c78fae428baeefef02b
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2023-04-17 10:42:29 +0700

    integrate constant to listCourse

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+import { course1, latteart, roasted, brew } from "../assets";
+
+export const listCourse = [
+  {
+    id: "basic-barista",
+    img: course1,
+    title: "Basic Barista",
+    content1:
+      "Our barista academy offers a comprehensive coffee-making program that covers everything from the basics of coffee brewing to the intricacies of latte art. Our program is designed to provide students with a true Italian and European coffee education, with a focus on the artistry and craftsmanship that goes into creating the perfect cup of coffee.",
+    content2:
+      "In our program, students will learn the fundamentals of espresso-based beverages, including shot pulling, milk frothing, and drink composition. They'll also explore the world of latte art, learning how to create beautiful designs using steamed milk. Our experienced instructors will guide students through the process, providing hands-on instruction and personalized feedback to help them hone their skills.",
+    date: "2 Days",
+    price: "229",
+  },
+  {
+    id: "latte-art",
+    img: latteart,
+    title: "Latte Art",
+    content1:
+      "Our latte art course is led by experienced baristas who are passionate about the art and craft of coffee-making. They will guide students through the process of creating latte art, from steaming the milk to pouring the perfect design. Students will learn the techniques for creating a range of designs, including hearts, rosettas, and tulips, as well as more complex designs such as swans and dragons.",
+    content2:
+      "In addition to hands-on instruction, our latte art course also covers the science behind latte art, including the chemistry of milk and the role of temperature and texture in creating beautiful designs. Students will leave the course with a deep understanding of the art and science of latte art, as well as the skills needed to create stunning designs in their lattes.",
+    date: "21 Day",
+    price: "129",
+  },
+  {
+    id: "coffee-brewing",
+    img: brew,
+    title: "Coffee Brewing",
+    content1:
+      "Our brewing course is led by experienced instructors who are passionate about the science and art of brewing coffee. They will guide students through the process of brewing coffee, from selecting the right beans to choosing the perfect brewing method. Students will learn about different brewing methods, such as pour-over, French press, and AeroPress, and gain a deep understanding of the science behind brewing coffee, including extraction, grind size, and water temperature.",
+    content2:
+      "In addition to hands-on instruction, our brewing course also covers the history and culture of coffee, from its origins in Ethiopia to its role in modern-day society. Students will gain a deep appreciation for the art and science of coffee-making and leave the course with the skills and knowledge needed to create the perfect cup of coffee every time.",
+    date: "1 Day",
+    price: "179",
+  },
+  {
+    id: "coffee-roster",
+    img: roasted,
+    title: "Coffee Roster",
+    content1:
+      "Our coffee roasting course is designed to teach students the art and science of coffee roasting. Led by experienced roasters, this hands-on course covers everything from green coffee selection to roasting profiles and beyond.",
+    content2:
+      "Students will learn about the different stages of coffee roasting, including drying, browning, and development, and gain a deep understanding of the chemical reactions that occur during the roasting process. They will also learn how to calibrate and use roasting equipment, as well as how to analyze roasted coffee for flavor and quality.",
+    date: "3 Days",
+    price: "549",
+  },
+];
